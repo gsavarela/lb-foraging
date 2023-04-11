3ac0d96fda60f861fd752bddf90eadbcc46125ef
Guilherme Varela <guilhermevarela@protonmail.com>
2023-04-11 16:31:06 +0100

    Add partial observability combination w multiple players

--- lbforaging/__init__.py
+++ lbforaging/__init__.py
@@ -8,16 +8,33 @@ coop = [True, False]
 partial_obs = [True, False]
 
 for s, p, f, c, po in product(sizes, players, foods, coop, partial_obs):
-    register(
-        id="Foraging{4}-{0}x{0}-{1}p-{2}f{3}-v1".format(s, p, f, "-coop" if c else "", "-2s" if po else ""),
-        entry_point="lbforaging.foraging:ForagingEnv",
-        kwargs={
-            "players": p,
-            "max_player_level": 3,
-            "field_size": (s, s),
-            "max_food": f,
-            "sight": 2 if po else s,
-            "max_episode_steps": 50,
-            "force_coop": c,
-        },
-    )
+    if s == 15 and p == 4 and f == 5 and po:
+        # Register more partial observability scopes
+        for i in (2, 5, 10, 12):
+            register(
+                id="Foraging{4}-{0}x{0}-{1}p-{2}f{3}-v1".format(s, p, f, "-coop" if c else "", f"-{i}s" if po else ""),
+                entry_point="lbforaging.foraging:ForagingEnv",
+                kwargs={
+                    "players": p,
+                    "max_player_level": 3,
+                    "field_size": (s, s),
+                    "max_food": f,
+                    "sight": i if po else s,
+                    "max_episode_steps": 50,
+                    "force_coop": c,
+                },
+            )
+    else:
+        register(
+            id="Foraging{4}-{0}x{0}-{1}p-{2}f{3}-v1".format(s, p, f, "-coop" if c else "", "-2s" if po else ""),
+            entry_point="lbforaging.foraging:ForagingEnv",
+            kwargs={
+                "players": p,
+                "max_player_level": 3,
+                "field_size": (s, s),
+                "max_food": f,
+                "sight": 2 if po else s,
+                "max_episode_steps": 50,
+                "force_coop": c,
+            },
+        )
